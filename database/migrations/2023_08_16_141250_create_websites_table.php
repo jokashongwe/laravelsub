5134d180438bf99e5cf51efb89132b6d9ab0f319
Jonathan Kashongwe <jokashongwe@gmail.com>
2023-08-16 15:26:28 +0100

    Add models and subscriptions

--- database/migrations/2023_08_16_141250_create_websites_table.php
+++ database/migrations/2023_08_16_141250_create_websites_table.php
@@ -13,8 +13,10 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('flights', function (Blueprint $table) {
+        Schema::create('websites', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('url');
             $table->timestamps();
         });
     }
@@ -26,6 +28,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('flights');
+        Schema::dropIfExists('websites');
     }
 };
---
+++ database/migrations/2023_08_16_141949_create_subscriptions_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Models\User;
+use App\Models\Website;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('subscriptions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId(User::class);
+            $table->foreignId(Website::class);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('subscriptions');
+    }
+};
